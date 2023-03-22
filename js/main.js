303d0a3ca7e29ca484ee421367257d59d39c858d
Marcin Lautsch <mldemigod@gmail.com>
2023-03-22 19:09:42 +0100

    webParadiseFinish

--- js/main.js
+++ js/main.js
@@ -1,50 +1,54 @@
 const nav = document.querySelector('.nav')
-const navBtn = document.querySelector('.burger-btn')
-const allNavItems = document.querySelectorAll('.nav__item')
+const btnBurger = document.querySelector('.burger-btn')
+const allNaviItems = document.querySelectorAll('.nav__item')
 const navBtnBars = document.querySelector('.burger-btn__bars')
-const allSections = document.querySelectorAll('section')
+const allSections = document.querySelectorAll('.section')
 const footerYear = document.querySelector('.footer__year')
+// =========
 
 const handleNav = () => {
 	nav.classList.toggle('nav--active')
-
 	navBtnBars.classList.remove('black-bars-color')
 
-	allNavItems.forEach(item => {
+	allNaviItems.forEach(item => {
 		item.addEventListener('click', () => {
 			nav.classList.remove('nav--active')
 		})
 	})
 
-	handleNavItemsAnimation()
+	handlenaviItemAnimation()
 }
 
-const handleNavItemsAnimation = () => {
+const handlenaviItemAnimation = () => {
 	let delayTime = 0
 
-	allNavItems.forEach(item => {
-		item.classList.toggle('nav-items-animation')
+	allNaviItems.forEach(item => {
+		item.classList.toggle('nav-item-animation')
 		item.style.animationDelay = '.' + delayTime + 's'
 		delayTime++
 	})
 }
+
+//zmieia date w footerze funkcja
+const handleCurrentYear = () => {
+	const year = new Date().getFullYear()
+	footerYear.innerText = year
+}
+
+//funkcja obesrwowanie sekcji zeby zmienic kolor burger menu
 const handleObserver = () => {
-	const currentSection = window.scrollY
+	const currentsection = window.scrollY
 
 	allSections.forEach(section => {
-		if (section.classList.contains('white-section') && section.offsetTop <= currentSection + 60) {
+		if (section.classList.contains('white-section') && section.offsetTop <= currentsection + 60) {
 			navBtnBars.classList.add('black-bars-color')
-		} else if (!section.classList.contains('white-section') && section.offsetTop <= currentSection + 60) {
+		} else if (!section.classList.contains('white-section') && section.offsetTop <= currentsection + 60) {
 			navBtnBars.classList.remove('black-bars-color')
 		}
 	})
 }
 
-const handleCurrentYear = () => {
-	let year = new Date().getFullYear()
-	footerYear.innerText = year
-}
-
+// wywołanie funkcji (właczenie).
+btnBurger.addEventListener('click', handleNav)
 handleCurrentYear()
-navBtn.addEventListener('click', handleNav)
 window.addEventListener('scroll', handleObserver)
